b43f44cfa55397274937be6a16d7ae21cf0e2154
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2021-05-27 22:00:48 -0400

    users can add ticket to database

--- hub/src/components/TicketForm.js
+++ hub/src/components/TicketForm.js
@@ -1,9 +1,62 @@
 import React, { useState, useEffect } from "react";
 import "../css/Tickets.scss";
 import Cancel from '../img/cancel.svg';
-
+import axios from 'axios';
 
 const TicketForm = (props) => {
+  const [values, setValues] = useState({
+    userid: localStorage.getItem('id'),
+    fullname: '',
+    ticketdescription: '',
+    phonenumber: '',
+    companyname: ''
+  });
+
+  const handleFullNameInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      fullname: event.target.value,
+    }));
+  };
+
+  const handleTicketDescriptionInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      ticketdescription: event.target.value,
+    }));
+  };
+
+
+  const handlePhoneInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      phonenumber: event.target.value,
+    }));
+  };
+
+  const handleCompanyNameInputChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      companyname: event.target.value,
+    }));
+  };
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.post("http://localhost:8886/addTicket.php", values)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  }
 
 
   return (
@@ -14,14 +67,14 @@ const TicketForm = (props) => {
                 {props.Closer}
                 
               </div>
-              <form className='tickets-container__tickets-header-top__button-container__ticket-creator__form'>
+              <form onSubmit={handleSubmit} className='tickets-container__tickets-header-top__button-container__ticket-creator__form'>
                 <div className='tickets-container__tickets-header-top__button-container__ticket-creator__form__label-input'>
                   <label>Ticket name *</label>
                   <input />
                 </div>
                 <div className='tickets-container__tickets-header-top__button-container__ticket-creator__form__label-input'>
                   <label>Ticket description</label>
-                  <input />
+                  <input onChange={handleTicketDescriptionInputChange}/>
                 </div>
                 <div className='tickets-container__tickets-header-top__button-container__ticket-creator__form__label-input'>
                   <label>Source </label>
@@ -43,17 +96,17 @@ const TicketForm = (props) => {
                     <h5>Associate ticket with</h5>
                     <div className='tickets-container__tickets-header-top__button-container__ticket-creator__form__label-input'>
                   <label>Company</label>
-                  <input />
+                  <input onChange={handleCompanyNameInputChange} />
                 </div>
                 <div className='tickets-container__tickets-header-top__button-container__ticket-creator__form__label-input'>
                   <label>Contact</label>
-                  <input />
+                  <input  onChange={handleFullNameInputChange}/>
                 </div>
 
                 </div>
 
                 <div className='tickets-container__tickets-header-top__button-container__ticket-creator__form__submit'>
-                  <button className='tickets-container__tickets-header-top__button-container__ticket-creator__form__submit__create'>Create</button>
+                  <button type='submit' className='tickets-container__tickets-header-top__button-container__ticket-creator__form__submit__create'>Create</button>
                   {props.FormCloser}
                 </div>
               </form>
